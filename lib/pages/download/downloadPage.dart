d42944e866cfbc6324cd08cec81ff1192002fbbf
will <545512533@qq.com>
2020-04-01 00:10:27 +0800

    添加下载页面

---
+++ lib/pages/download/downloadPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+/// 下载页面
+class DownloadPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    throw _DownloadPageState();
+  }
+
+}
+
+class _DownloadPageState extends State<DownloadPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw Container();
+  }
+}
